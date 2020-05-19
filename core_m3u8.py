23bd2f2608b2c53b296a110d3edcf27575d7f5be
gzlock <gzlock88@gmail.com>
2020-05-19 17:47:29 +0800

    20200519

--- core_m3u8.py
+++ core_m3u8.py
@@ -1,62 +1,22 @@
-import threading
-import time
-from typing import Callable, List
 from urllib import parse
 
-import aria2p
 import m3u8
 import requests
 
-from ui_log import Frame
-
-aria2_client_p: aria2p.API
-
-is_stop: bool = False
-
-
-class NotM3u8Exception(Exception):
-    pass
-
 
 class CoreM3u8:
-    def __init__(self, url: str, log: Frame, m3u8_proxy: str, callback: Callable[[List], None]):
+    def __init__(self, url: str, m3u8_proxy: str):
         self.__url = url
-        self.__m3u8_proxy = m3u8_proxy
-        self.__log = log
-        self.__is_running: bool = False
-        self.__callback = callback
-        self.urls: List[str] = []
+        self.__proxy = m3u8_proxy
 
-    def start(self):
-        self.__is_running = True
-        threading.Thread(target=self.__loop).start()
-
-    def stop(self):
-        self.__is_running = False
-
-    def __loop(self):
-        downloaded_url: set = set()
-        while self.__is_running:
-            try:
-                new = self.load_m3u8_content()
-                unique = list(new - downloaded_url)
-                downloaded_url |= new
-                # print('新url', unique)
-                if len(unique) > 0:
-                    self.__callback(unique)
-            except Exception as e:
-                self.__log.error('m3u8错误\n{}'.format(e))
-            finally:
-                time.sleep(2)
-
-    def load_m3u8_content(self) -> set:
+    def load_m3u8_content(self, timeout: int = 5) -> set:
         print('load_m3u8_content', self.__url)
-        res = requests.get(url=self.__url, proxies={'http': self.__m3u8_proxy, 'https': self.__m3u8_proxy},
-                           timeout=5)
+        res = requests.get(url=self.__url, proxies={'http': self.__proxy, 'https': self.__proxy},
+                           timeout=timeout)
         if res.status_code != 200:
-            raise Exception('m3u8 response status code {}'.format(res.status_code))
+            raise Exception('m3u8源响应状态不等于200，当前为：{}'.format(res.status_code))
         if '#EXTM3U' not in res.text:
-            raise NotM3u8Exception('不是标准的HLS m3u8文档')
+            raise Exception('m3u8源不是标准的HLS m3u8文档')
         content = m3u8.loads(res.text, uri=self.__url)
         '''可变分辨率，取最大分辨率的m3u8链接'''
         if content.is_variant:
